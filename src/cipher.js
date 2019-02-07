7ab6f02fa7cc662d3a3e1915ce33ef6aad757f9b
aleisabl <aleisabl.25@gmail.com>
2019-02-07 12:11:01 -0300

    first commit, agregando código en VSCode a mi repositorio

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,33 @@
 window.cipher = {
   encode: () => {
-    /* Acá va tu código */
+      let resultEncode = "";
+      let text = document.getElementById('message').value.toUpperCase();
+      let offset = document.getElementById('offsetNumber').value;
+      
+      for (let i=0; i<text.length; i++){
+          let formula = ((text.charCodeAt(i)-65+ parseInt(offset))%26+65);
+          resultEncode += String.fromCharCode(formula);
+          
+      }
+      if(document.getElementById("message").value.length == 0){
+          alert("Por favor rellenar todos los campos");
+      }
+      document.getElementById("showEncode").innerHTML = `Mensaje ocultado: ${resultEncode}`;
   },
+
   decode: () => {
-    /* Acá va tu código */
+      let resultDecode = "";
+      let textDecode = document.getElementById('message').value.toUpperCase();
+      let offset = document.getElementById('offsetNumber').value;
+  
+      for (let i=0; i<textDecode.length; i++){
+  
+          let formulaDecode = ((textDecode.charCodeAt(i)-90- parseInt(offset))%26+90);
+          resultDecode += String.fromCharCode(formulaDecode);   
+      }
+      if(document.getElementById("message").value.length == 0){
+          alert("Por favor rellenar todos los campos");
+      }
+      document.getElementById("showDecode").innerHTML = `Mensaje revelado: ${resultDecode}`;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 /* Acá va tu código */
+window.onload = () => {
+    /* add event listener para que el usuario no pueda colocar cualquier número en el input
+    y utilice sólo las flechas */
+    const mouseOnly = document.getElementById("offsetNumber");
+    mouseOnly.addEventListener("keypress", (event) => {
+      event.preventDefault();
+    });
+  }
