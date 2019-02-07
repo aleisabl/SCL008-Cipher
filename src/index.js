98a3210b7ec5ebbad50dd56f167bedba24c3f8c0
aleisabl <aleisabl.25@gmail.com>
2019-02-07 13:06:35 -0300

    falta conectar con el dom

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-/* Acá va tu código */
-window.onload = () => {
-    /* add event listener para que el usuario no pueda colocar cualquier número en el input
+/* add event listener para que el usuario no pueda colocar cualquier número en el input
     y utilice sólo las flechas */
+window.onload = () => {
     const mouseOnly = document.getElementById("offsetNumber");
     mouseOnly.addEventListener("keypress", (event) => {
       event.preventDefault();
     });
   }
+
+  /*  if(document.getElementById("message").value.length == 0){
+            alert("Por favor rellenar todos los campos");
+        } */
+
+        /* if(document.getElementById("message").value.length == 0){
+            alert("Por favor rellenar todos los campos");
+        } */
